08700e778651011ff871010c09c1840af97d853d
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-09-13 21:29:02 +0900

    [level 0] Title: 대문자와 소문자, Time: 19.09 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+            char ch = my_string.charAt(i);
+            if(65<=ch && ch<=90) {
+                answer += (char) (ch + 32);
+            } else if (97<=ch && ch<=122) {
+                answer += (char) (ch - 32);
+            }
+        }
+        return answer;
+    }
+}
