82b927381c163504a5b95ea27ccd4f824f44b3ce
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-03 22:05:40 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.21 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -2,16 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer;
-        if (num_list.length % n == 0) {
-            answer = new int[num_list.length / n];
-        } else {
-            answer = new int[num_list.length / n + 1];
-        }
-        
-        int index = 0;
-        for (int i=0; i<num_list.length; i+=n) {
-            answer[index++] = num_list[i];
+        int[] answer = new int[(int)Math.ceil((num_list.length / (n * 1.0)))];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = num_list[i * n];
         }
         return answer;
     }
