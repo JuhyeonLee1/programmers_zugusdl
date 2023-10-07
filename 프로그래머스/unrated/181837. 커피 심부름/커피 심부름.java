78c58d3d806d62a3567c1482c1467184b389e8e3
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-07 23:12:25 +0900

    [unrated] Title: 커피 심부름, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         for(int i=0; i<order.length; i++) {
-            if(order[i].contains("americano") || order[i].contains("anything")) {
+            if(order[i].indexOf("americano") != -1 || order[i].indexOf("anything") != -1) {
                 answer += 4500;
-            } else if (order[i].contains("cafelatte")) {
+            } else if (order[i].indexOf("cafelatte") != -1) {
                 answer += 5000;
             }
         }
