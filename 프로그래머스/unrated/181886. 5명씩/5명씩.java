0c827ad99ae7effb165e537b93c0cc986cd604e4
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-08 20:58:27 +0900

    [unrated] Title: 5명씩, Time: 0.20 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -2,10 +2,8 @@ class Solution {
     public String[] solution(String[] names) {
         int length = (int)Math.ceil((double)names.length/5);
         String[] answer = new String[length];
-        int idx = 0;
-        for(int i=0; i<names.length; i+=5) {
-            answer[idx] = names[i];
-            idx++;
+        for(int i=0; i<length; i++) {
+            answer[i] = names[i*5];
         }
         return answer;
     }
