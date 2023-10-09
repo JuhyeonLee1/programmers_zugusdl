af21a977e37be43d08dd3509748aacbbf77480db
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-09 18:33:27 +0900

    [level unrated] Title: k의 개수, Time: 29.92 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.java
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
+        String k1 = String.valueOf(k);
         for(; i<=j; i++) {
-            if((i+"").indexOf(k+"") != -1) {
-                String[] str = (i+"").split("");
+            String i1 = String.valueOf(i);
+            if(i1.indexOf(k1) != -1) {
+                String[] str = i1.split("");
                 for(int a=0; a<str.length; a++) {
-                    if(str[a].equals(k+"")) {
+                    if(str[a].equals(k1)) {
                         answer++;
                     }
                 }
