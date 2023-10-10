a9f752eec629e9a86ec98960a175da706d4cec15
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-10 21:51:59 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -2,10 +2,9 @@ class Solution {
     public int solution(int num, int k) {
         int answer = -1;
         String n = String.valueOf(num);
-        String strK = String.valueOf(k);
         for(int i=0; i<n.length(); i++) {
-            if(Character.toString(n.charAt(i)).equals(strK)) {
-                answer = n.indexOf(strK) + 1;
+            if(n.charAt(i) == k + '0') {
+                answer = i + 1;
                 break;
             }
         }
