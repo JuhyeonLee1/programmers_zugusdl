2499a4052e80aed3affeaae79e0d17b1a2c52610
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-11 23:27:49 +0900

    [level unrated] Title: 문자열 잘라서 정렬하기, Time: 19.48 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        String[] str = myString.split("x");
+        ArrayList<String> str_list = new ArrayList<String>();
+        for(int i=0; i<str.length; i++) {
+            if(!str[i].isEmpty()) {
+                str_list.add(str[i]);
+            }
+        }
+        String[] answer = new String[str_list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = str_list.get(i);
+        }
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
