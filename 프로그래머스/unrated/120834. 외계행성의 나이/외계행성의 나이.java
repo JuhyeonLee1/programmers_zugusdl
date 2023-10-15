1294a7c8a520020b64fcbd384539290f8fcadfc3
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-15 22:53:00 +0900

    [level unrated] Title: 외계행성의 나이, Time: 13.78 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,18 +3,7 @@ class Solution {
         String answer = "";
         String a = age + "";
         for(int i=0; i<a.length(); i++) {
-            switch(a.charAt(i)) {
-                case '0': answer += "a"; break;
-                case '1': answer += "b"; break;
-                case '2': answer += "c"; break;
-                case '3': answer += "d"; break;
-                case '4': answer += "e"; break;
-                case '5': answer += "f"; break;
-                case '6': answer += "g"; break;
-                case '7': answer += "h"; break;
-                case '8': answer += "i"; break;
-                case '9': answer += "j"; break;
-            }
+            answer += (char)(a.charAt(i)+49);
         }
         return answer;
     }
