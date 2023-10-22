d0aae326e2f3acd8a33252be8deef885d678ac29
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-10-22 18:52:20 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 16.27 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(!s.equals("1")) {
+            answer[1] += s.length() - s.replace("0", "").length();
+            s = s.replace("0", "");
+            s = Integer.toBinaryString(s.length());
+            answer[0]++;
+        }
+        return answer;
+    }
+}
