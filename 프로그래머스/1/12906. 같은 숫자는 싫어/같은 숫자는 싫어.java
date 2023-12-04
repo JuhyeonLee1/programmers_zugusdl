cef11e97f7cd642f2e83bee9e72613aab14fa7ef
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-12-04 22:52:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.46 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,13 +3,9 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         List<Integer> list = new ArrayList<Integer>();
-		
-		for(int i=0; i<arr.length; i++) {
-			if(i > 0) {
-				if(arr[i] != arr[i-1]) {
-					list.add(arr[i]);
-				}
-			} else {
+		list.add(arr[0]);
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i] != arr[i-1]) {
 				list.add(arr[i]);
 			}
 		}
