8f6d7119769b47dc2e93527c75905e9ac39a14ea
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-12-05 23:28:51 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.17 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[^0-9]", " ");
+
+        String[] strArr = my_string.split(" ");
+        
+        for(String s : strArr) {
+        	if(!s.equals("")) {
+        		answer += Integer.parseInt(s);
+        	}
+        }
+        
+        return answer;
+    }
+}
