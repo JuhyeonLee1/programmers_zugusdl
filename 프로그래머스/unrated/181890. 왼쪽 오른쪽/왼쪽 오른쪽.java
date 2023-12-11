c8e3c6d79312d9c787ea202ac79b3e01ea8db862
zugusdl <139878100+zugusdl@users.noreply.github.com>
2023-12-11 21:54:19 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,23 +2,18 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        List<String> sList = new ArrayList<String>();
+        String[] answer = {};
         
         for(int i=0; i<str_list.length; i++) {
             if(str_list[i].equals("l")) {
-                for(int j=0; j<i; j++) {
-                    sList.add(str_list[j]);
-                }
+                answer = Arrays.copyOfRange(str_list,0,i);
                 break;
             }
             if(str_list[i].equals("r")) {
-                for(int j=i+1; j<str_list.length; j++ ) {
-                    sList.add(str_list[j]);
-                }
+                answer = Arrays.copyOfRange(str_list,i+1,str_list.length);
                 break;
             }
         }
-        String[] answer = sList.toArray(new String[sList.size()]);
         
         return answer;
     }
