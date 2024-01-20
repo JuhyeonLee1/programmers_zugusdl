e56b4399f66067518b3ebe9de1fda30003a82c37
zugusdl <139878100+zugusdl@users.noreply.github.com>
2024-01-20 21:16:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.14 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int count = 0;
+
+        while(true){
+            count++;
+            stack.add(n%3);
+            n = n / 3;
+            if(n == 0) {
+                break;
+            }
+        }
+
+        for(int i=0; i<count; i++){
+            answer += stack.pop() * (Math.pow(3,i));
+        }
+
+        return answer;
+    }
+}
