cc53c93bf4404a0c3e776a3a7ab1fcfc22f52fe8
Juhyeon Lee <139878100+zugusdl@users.noreply.github.com>
2024-01-29 23:03:27 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int count = 0;
         while(n >= a) {
-            count = n / a;
-            n = n - (count * a) + (count * b);
-            answer += count * b;
-            count = 0;
+            n = n - a + b;
+            answer++;
         }
-        return answer;
+        return answer * b;
     }
 }
